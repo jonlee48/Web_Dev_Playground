ae28c583619784d576d66c06bda96a00875722af
jonlee48 <jonathan.lee48@gmail.com>
2019-07-14 20:52:48 -0400

    Master website all linked up

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ let toggleNav = function() {
   if (toggleNavStatus == false) {
     getSidebarUl.style.visibility = "visible";
     getSidebar.style.width = "272px";
-    getSidebarTitle.style.opacity = "0.5";
+    getSidebarTitle.style.opacity = "1";
 
     let arrayLength = getSidebarLinks.length;
     for (let i = 0; i < arrayLength; i++) {
